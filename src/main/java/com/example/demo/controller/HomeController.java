6a9326d8dac75df0e464f57e8679a3cfb5e3519f
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-07-31 20:40:52 +0200

    feat: controller

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.time.LocalTime;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping("/greeting")
+    
+    public String greeting(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        model.addAttribute("time", LocalTime.now());
+        return "greeting";
+    }
+    
+    @GetMapping("/saluti")
+    
+    public String saluti() {
+  
+        return "saluti";
+    }
+}
